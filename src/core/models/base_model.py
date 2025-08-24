78de04af62e6d5b54c7132e499ac391759302197
Muhinhassan26 <muhinhassan123@gmail.com>
2025-08-24 17:44:36 +0600

    Add BaseModel class with timestamp fields for database models

---
+++ src/core/models/base_model.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+from src.core.db import Base
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), onupdate=func.now(), server_default=func.now(), nullable=False
+    )
